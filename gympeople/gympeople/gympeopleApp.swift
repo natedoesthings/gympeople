06511981dd88bb19c743e8730cdcf555ec59e545
natedoesthings <nathanaeldtesfaye@gmail.com>
2025-11-11 23:01:02 -0600

    ios development setup

---
+++ gympeople/gympeople/gympeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  gympeopleApp.swift
+//  gympeople
+//
+//  Created by Nathanael Tesfaye on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct gympeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
